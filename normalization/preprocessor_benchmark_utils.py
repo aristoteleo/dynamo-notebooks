fbca10616e5cdbd137bfd78974c2e7c435301f71
user_name <dummtindex@gmail.com>
2021-12-19 00:44:49 -0500

    update benchmark dynamics

--- normalization/preprocessor_benchmark_utils.py
+++ normalization/preprocessor_benchmark_utils.py
@@ -1,3 +1,6 @@
+import time
+from anndata import AnnData
+from dynamo.preprocessing import Preprocessor
 from typing import Callable
 import dynamo as dyn
 import seaborn as sns
@@ -7,9 +10,7 @@ import numpy as np
 from dynamo.configuration import DKM
 import warnings
 warnings.filterwarnings('ignore')
-from dynamo.preprocessing import Preprocessor
-from anndata import AnnData
-import time
+
 
 def get_clean_organoid_data():
     adata = dyn.sample_data.scEU_seq_organoid()
@@ -18,39 +19,62 @@ def get_clean_organoid_data():
     adata.obs['time'] = adata.obs['time'].astype(float)
     adata = adata[adata.obs.time != -1, :]
     adata = adata[adata.obs.exp_type == 'Pulse', :]
-    adata.layers['new'], adata.layers['total'] = adata.layers['ul'] + adata.layers['sl'], adata.layers['su'] + adata.layers['sl'] + adata.layers['uu'] + adata.layers['ul']
+    adata.layers['new'], adata.layers['total'] = adata.layers['ul'] + \
+        adata.layers['sl'], adata.layers['su'] + \
+        adata.layers['sl'] + adata.layers['uu'] + adata.layers['ul']
     del adata.layers['uu'], adata.layers['ul'], adata.layers['su'], adata.layers['sl']
     return adata
 
 
 def get_clean_rp1_data():
     adata = dyn.sample_data.scEU_seq_rpe1()
-    rpe1_kinetics = adata[adata.obs.exp_type=='Pulse', :]
+    rpe1_kinetics = adata[adata.obs.exp_type == 'Pulse', :]
     rpe1_kinetics.obs['time'] = rpe1_kinetics.obs['time'].astype(str)
     rpe1_kinetics.obs.loc[rpe1_kinetics.obs['time'] == 'dmso', 'time'] = -1
     rpe1_kinetics.obs['time'] = rpe1_kinetics.obs['time'].astype(float)
     rpe1_kinetics = rpe1_kinetics[rpe1_kinetics.obs.time != -1, :]
 
-    rpe1_kinetics.layers['new'], rpe1_kinetics.layers['total'] = rpe1_kinetics.layers['ul'] + rpe1_kinetics.layers['sl'], rpe1_kinetics.layers['su'] + rpe1_kinetics.layers['sl'] + rpe1_kinetics.layers['uu'] + rpe1_kinetics.layers['ul']
+    rpe1_kinetics.layers['new'], rpe1_kinetics.layers['total'] = rpe1_kinetics.layers['ul'] + \
+        rpe1_kinetics.layers['sl'], rpe1_kinetics.layers['su'] + \
+        rpe1_kinetics.layers['sl'] + \
+        rpe1_kinetics.layers['uu'] + rpe1_kinetics.layers['ul']
 
     del rpe1_kinetics.layers['uu'], rpe1_kinetics.layers['ul'], rpe1_kinetics.layers['su'], rpe1_kinetics.layers['sl']
     return rpe1_kinetics
 
 
-def recipe_benchmark(adata: AnnData, recipe:str, tkey=None):
+def get_clean_chromaffin_data():
+    adata = dyn.sample_data.chromaffin()
+    adata.obs["time"] = 1
+    return adata
+
+
+def recipe_benchmark(adata: AnnData, recipe: str, tkey=None):
     preprocessor = Preprocessor()
     preprocessor.preprocess_adata(adata, recipe=recipe, tkey=tkey)
 
 
-def benchmark_after_pp(adata: AnnData, color: str, file_prefix="pp"):
-    dyn.tl.reduceDimension(adata,basis="pca")
-    dyn.tl.reduceDimension(adata,basis="pca")
+def benchmark_umap(adata: AnnData, color: str, file_prefix="pp"):
+    dyn.tl.reduceDimension(adata, basis="pca")
+    dyn.tl.reduceDimension(adata, basis="pca")
     dyn.pl.umap(adata, color=color, figsize=(10, 10))  # , ax=axes[0])
     plt.savefig("_".join([file_prefix, "umap.png"]))
 
 
-def benchmark_all_recipes(data_generator: Callable, color, save_dir="./", tkey=None):
-    recipes = ["monocle", "seurat", "sctransform", "pearson_residuals"]
+def benchmark_dynamics(adata: AnnData, color: str, file_prefix="pp", dynamics_kwargs = {}):
+    dyn.tl.dynamics(adata, cores=16, **dynamics_kwargs)
+    dyn.tl.cell_velocities(adata)
+    dyn.pl.streamline_plot(adata, color=color)
+
+
+def benchmark_all_recipes(data_generator: Callable,
+                          color, 
+                          recipes=["monocle", "seurat",
+                                    "pearson_residuals", "sctransform"],
+                            dynamics_kwargs = {}, 
+                          save_dir="./", tkey=None, dataset_name="pancreas"):
+
+    do_dynamics_benchmarks = ["monocle", "seurat", "pearson_residuals"]
     recipe2time = {}
     for recipe in recipes:
         adata = data_generator()
@@ -58,5 +82,7 @@ def benchmark_all_recipes(data_generator: Callable, color, save_dir="./", tkey=N
         recipe_benchmark(adata, recipe=recipe, tkey=tkey)
         duration = time.time() - start_time
         recipe2time[recipe] = duration
-        benchmark_after_pp(adata, color=color, file_prefix="pancreas")
+        benchmark_umap(adata, color=color, file_prefix=dataset_name)
+        if recipe in do_dynamics_benchmarks:
+            benchmark_dynamics(adata, color=color, dynamics_kwargs=dynamics_kwargs)
     return recipe2time
